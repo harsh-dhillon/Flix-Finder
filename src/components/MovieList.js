74314e7f874dad8f5c5e85fc2055bb19de515ded
Harsh Dhillon <harshsdhillon13@gmail.com>
2023-04-25 12:47:57 -0400

    Updates

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -21,7 +21,17 @@ function getMovieDetails(movie) {
 }
 
 function MovieList({ movieRecommendations }) {
-  if (movieRecommendations.length === 0) {
+  const movieTitles = movieRecommendations
+      .flatMap((recommendation) =>
+          recommendation
+              .split("\n")
+              .map((movie) => movie.trim())
+              .filter((movie) => movie !== "")
+              .map((movie) => getMovieDetails(movie))
+      )
+      .filter((movie) => movie !== null && movie.title && movie.year);
+
+  if (movieTitles.length === 0) {
     return (
         <p className="text-gray-500 font-semibold">
           No movie recommendations found.
@@ -30,35 +40,16 @@ function MovieList({ movieRecommendations }) {
   }
 
   return (
-      <>
-        {movieRecommendations.map((recommendation) => {
-          const movieList = recommendation
-              .split("\n")
-              .map((movie) => movie.trim())
-              .filter((movie) => movie !== "");
-          const movieTitles = movieList
-              .map((movie) => getMovieDetails(movie))
-              .filter((movie) => movie !== null && movie.title && movie.year);
-
-          if (movieTitles.length === 0) {
-            return null;
-          }
-
-          return (
-              <React.Fragment key={movieTitles[0].title}>
-                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center">
-                  {movieTitles.slice(0).map(({ title, year }, index) => (
-                      <div key={index}>
-                        <MovieCard movieTitle={title} releaseYear={year} />
-                      </div>
-                  ))}
-                </div>
-              </React.Fragment>
-          );
-        })}
-      </>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center">
+        {movieTitles.map(({ title, year }, index) => (
+            <div key={index}>
+              <MovieCard movieTitle={title} releaseYear={year} />
+            </div>
+        ))}
+      </div>
   );
 }
+
 
 
 MovieList.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,31 +5,40 @@ import MovieList from "@/components/MovieList";
 import LoadingBar from "react-top-loading-bar";
 
 export default function App() {
-  const [movieRecommendations, setMovieRecommendations] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [progress, setProgress] = useState(0);
+    const [movieRecommendations, setMovieRecommendations] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [progress, setProgress] = useState(0);
 
-  useEffect(() => {
-    const storedRecommendations = localStorage.getItem("movieRecommendations");
-    if (storedRecommendations) {
-      setMovieRecommendations(JSON.parse(storedRecommendations));
-    }
-  }, []);
+    useEffect(() => {
+        const storedRecommendations = localStorage.getItem("movieRecommendations");
+        if (storedRecommendations) {
+            setMovieRecommendations(JSON.parse(storedRecommendations));
+        }
+    }, []);
 
-  const handleSubmit = (recommendations) => {
-    setProgress(30);
-    setIsLoading(true);
+    useEffect(() => {
+        if (!isLoading) {
+            const scrollY = window.scrollY;
+            setTimeout(() => {
+                window.scrollTo(0, scrollY);
+            }, 0);
+        }
+    }, [isLoading]);
 
-    setTimeout(() => {
-      setMovieRecommendations(recommendations);
-      setIsLoading(false);
-      setProgress(100);
-      localStorage.setItem(
-          "movieRecommendations",
-          JSON.stringify(recommendations)
-      );
-    }, 1000);
-  };
+    const handleSubmit = (recommendations) => {
+        setProgress(30);
+        setIsLoading(true);
+
+        setTimeout(() => {
+            setMovieRecommendations(recommendations);
+            setIsLoading(false);
+            setProgress(100);
+            localStorage.setItem(
+                "movieRecommendations",
+                JSON.stringify(recommendations)
+            );
+        }, 1000);
+    };
 
     return (
         <div className="container mx-auto my-3 px-10 sm:px-8 lg:max-w-7xl">
@@ -37,7 +46,6 @@ export default function App() {
                 <title>Flix Finder</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-
             <LoadingBar
                 className="rainbow-progress"
                 height={5}
@@ -49,13 +57,10 @@ export default function App() {
                 <h1 className="text-3xl sm:text-4xl font-bold m-5">Flix Finder</h1>
 
                 <MovieForm onSubmit={handleSubmit} />
-                <div className="container lg:max-w-7xl">
+                <div className="container lg:max-w-7xl" style={{ minHeight: "330px" }}>
                     <MovieList movieRecommendations={movieRecommendations} />
                 </div>
             </main>
         </div>
     );
-
-
-
 }
